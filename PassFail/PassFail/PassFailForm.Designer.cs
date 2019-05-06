381f9a8f70e99421351baab91b118b406d617e97
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-05-06 12:07:07 -0400

    finished pass fail

---
+++ PassFail/PassFail/PassFailForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PassFail
+{
+    partial class frmPassFail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnParseFile
+            // 
+            this.btnParseFile.Font = new System.Drawing.Font("Times New Roman", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParseFile.Location = new System.Drawing.Point(47, 97);
+            this.btnParseFile.Name = "btnParseFile";
+            this.btnParseFile.Size = new System.Drawing.Size(714, 169);
+            this.btnParseFile.TabIndex = 0;
+            this.btnParseFile.Text = "Parse File";
+            this.btnParseFile.UseVisualStyleBackColor = true;
+            this.btnParseFile.Click += new System.EventHandler(this.btnParseFile_Click);
+            // 
+            // frmPassFail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 448);
+            this.Controls.Add(this.btnParseFile);
+            this.Name = "frmPassFail";
+            this.Text = "Pass Fail Zach K";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParseFile;
+    }
+}
+
---
+++ PassFail/PassFail/PassFailForm.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Created by: Zachary Kranabetter
+ * Created on: May 2nd 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #41 - Pass Fail
+ * This program parses an input file to figure out 
+ * how many students got an average of over 50%
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassFail
+{
+    public partial class frmPassFail : Form
+    {
+        // declare file paths
+        string filepath = "DATA10.txt";
+        string outputFile = "DATA10_Output.txt";
+
+        // declare variables
+        int counterA, counterB;
+        float weightTests, weightAssign, weightProjects, weightQuizzes;
+        float markTests, markAssign, markProjects, markQuizzes;
+        int numPassed, numStudents;
+        float average;
+
+        public frmPassFail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParseFile_Click(object sender, EventArgs e)
+        {
+            // read each line of the file into a string array
+            // each element of the array is one line in the file
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+
+            // loop through each line of the array of lines
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(outputFile))
+            {
+                // loop through each line of the array
+                for (counterA = 0; counterA < lines.Length; counterA++)
+                {
+                    // initialize number of students who passed
+                    numPassed = 0;
+
+                    // read the first line for the next set of students
+                    string line = lines[counterA];
+
+                    // split each weight by the space delimeter
+                    string[] weights = line.Split(' ');
+
+                    // set the weights for next set of students
+                    weightTests = float.Parse(weights[0]);
+                    weightAssign = float.Parse(weights[1]);
+                    weightProjects = float.Parse(weights[2]);
+                    weightQuizzes = float.Parse(weights[3]);
+
+                    // get the number of students from the array
+                    numStudents = int.Parse(lines[counterA + 1]);
+
+                    // loop through each student
+                    for (counterB = counterA + 2; counterB < counterA + 2 + numStudents; counterB++)
+                    {
+                        // read the next line into array
+                        string[] studentMarks = lines[counterB].Split(' ');
+
+                        // convert the students grades to numbers
+                        markTests = float.Parse(studentMarks[0]);
+                        markAssign = float.Parse(studentMarks[1]);
+                        markProjects = float.Parse(studentMarks[2]);
+                        markQuizzes = float.Parse(studentMarks[3]);
+
+                        // calculate the average of the student
+                        average = (markTests * (weightTests / 100)) + (markAssign * (weightAssign / 100)) + (markProjects * (weightProjects / 100)) + (markQuizzes * (weightQuizzes / 100));
+
+                        // if they have a mark higher or equal to 50 add 1 to the number of students that passed
+                        if (average >= 50)
+                        {
+                            numPassed++;
+                        }
+                    }
+
+                    // reset the initial counter to start at the next batch of students
+                    counterA = counterB - 1;
+
+                    // output the numPassed
+                    file.WriteLine(numPassed);
+                }
+            }
+        }
+    }
+}
