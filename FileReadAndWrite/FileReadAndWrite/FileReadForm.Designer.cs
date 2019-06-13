cf45356a6ec9252710480d18d30a691d779b0108
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-06-13 13:13:13 -0400

    Made file read and write

---
+++ FileReadAndWrite/FileReadAndWrite/FileReadForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FileReadAndWrite
+{
+    partial class FileReadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(102, 147);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(99, 198);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(13, 13);
+            this.lblAnswer.TabIndex = 1;
+            this.lblAnswer.Text = "?";
+            // 
+            // FileReadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "FileReadForm";
+            this.Text = "File read & write Zach K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FileReadAndWrite/FileReadAndWrite/FileReadForm.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by: Zachary Kranabetter
+ * Created on: May 1st 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #40 - String Equality
+ * This program lets the user compares strings from an input file and determine if they are the 
+ * same, then in an output file it will say true or false for eacdh line
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReadAndWrite
+{
+    public partial class FileReadForm : Form
+    {
+        public FileReadForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool StringsAreEqual(string case1, string case2)
+        {
+            // make the boolean variable
+            bool Equal;
+
+            // check if the strings have the same number of characters
+            if (case1.Length == case2.Length)
+            {
+                // make the strings lowercase
+                case1 = case1.ToLower();
+                case2 = case2.ToLower();
+
+                if (case1 == case2)
+                {
+                    // set equal to true
+                    Equal = true;
+                }
+                else
+                {
+                    // set equal to true
+                    Equal = false;
+                }
+            }
+            else
+            {
+                // set equal to flase
+                Equal = false;
+            }
+            
+            return Equal;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // create my array of lines and set it to the input file
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            // create the seperators to read the input
+            char[] charSeperators = new char[] { ' ', '\t' };
+
+            string output = "";
+
+            // go though each line and check if the strings are the same 
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(charSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (StringsAreEqual(words[0], words[1]) == true)
+                {
+                    output = output + "True\r\n";
+                }
+                else
+                {
+                    output = output + "False\r\n";
+                }
+            }
+
+            // display it
+            System.IO.File.WriteAllText(@"output.txt", output);
+
+            this.lblAnswer.Show();
+
+        }
+    }
+}
